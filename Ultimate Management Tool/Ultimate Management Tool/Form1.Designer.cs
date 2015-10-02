53595af188bf67671e1d0ba46d29be4485204838
bartekordek <bartekordek@gmail.com>
2015-10-02 17:08:29 +0200

    Changes in layout - added two buttons.

--- Ultimate Management Tool/Ultimate Management Tool/Form1.Designer.cs
+++ Ultimate Management Tool/Ultimate Management Tool/Form1.Designer.cs
@@ -28,37 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(268, 249);
-            this.treeView1.TabIndex = 0;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(289, 13);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(698, 247);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1002, 273);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.treeView1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.btnLoadDb = new System.Windows.Forms.Button();
+			this.btnConnect2Server = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(12, 86);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(268, 249);
+			this.treeView1.TabIndex = 0;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(289, 87);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(698, 247);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// btnLoadDb
+			// 
+			this.btnLoadDb.Location = new System.Drawing.Point(13, 13);
+			this.btnLoadDb.Name = "btnLoadDb";
+			this.btnLoadDb.Size = new System.Drawing.Size(99, 21);
+			this.btnLoadDb.TabIndex = 2;
+			this.btnLoadDb.Text = "Load database";
+			this.btnLoadDb.UseVisualStyleBackColor = true;
+			this.btnLoadDb.Click += new System.EventHandler(this.btnLoadDb_Click);
+			// 
+			// btnConnect2Server
+			// 
+			this.btnConnect2Server.Location = new System.Drawing.Point(118, 13);
+			this.btnConnect2Server.Name = "btnConnect2Server";
+			this.btnConnect2Server.Size = new System.Drawing.Size(99, 21);
+			this.btnConnect2Server.TabIndex = 3;
+			this.btnConnect2Server.Text = "Connect to server";
+			this.btnConnect2Server.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1002, 347);
+			this.Controls.Add(this.btnConnect2Server);
+			this.Controls.Add(this.btnLoadDb);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.treeView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
@@ -66,6 +89,8 @@
 
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.DataGridView dataGridView1;
-    }
+		private System.Windows.Forms.Button btnLoadDb;
+		private System.Windows.Forms.Button btnConnect2Server;
+	}
 }
 
