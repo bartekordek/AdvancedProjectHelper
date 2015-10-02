4bd515b85e38751d562ee2343458240b3ed1a2af
bartekordek <bartekordek@gmail.com>
2015-10-02 17:07:48 +0200

    Added db manager class.

---
+++ Ultimate Management Tool/Ultimate Management Tool/CDbManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Ultimate_Management_Tool
+{
+	class CDbManager
+	{
+		public CDbManager()
+		{
+			dbConnection = null;
+        }
+
+		public void CreateConnection(String dbFilePath, Boolean isFileSystem = true)
+		{
+			SetDbFilePath( dbFilePath );
+            if( true == isFileSystem )
+			{
+
+			}
+		}
+
+		private void CreateConnection2File()
+		{
+
+		}
+
+		private void SetDbFilePath(String dbFilePath)
+		{
+			dbPath = dbFilePath;
+        }
+
+		private Boolean isOnFileSystem;
+		private String dbPath;
+		private SQLiteConnection dbConnection;
+	}
+}
