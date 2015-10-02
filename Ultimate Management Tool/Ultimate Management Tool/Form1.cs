d7ee7b8aeca05b602633804c86a42dc5154893f1
bartekordek <bartekordek@gmail.com>
2015-10-02 17:08:41 +0200

    Removed old db code.

--- Ultimate Management Tool/Ultimate Management Tool/Form1.cs
+++ Ultimate Management Tool/Ultimate Management Tool/Form1.cs
@@ -15,36 +15,11 @@ namespace Ultimate_Management_Tool
         public Form1()
         {
             InitializeComponent();
-            this.FillTree();
         }
 
-        private void FillTree()
-        {
-            using (var db = new UMTEntities())
-            {
-                var query = from a in db.Activities
-                            select a;
+		private void btnLoadDb_Click( object sender, EventArgs e )
+		{
 
-                foreach (var act in query)
-                {
-                    var node = new TreeNode()
-                    {
-                        Name=act.Id.ToString(),
-                        Text=act.activityName
-                    };
-
-                    if (act.parentActivityId.HasValue)
-                    {
-                        var parentNode = treeView1.Nodes.Find(act.parentActivityId.ToString(), true)[0];
-
-                        parentNode.Nodes.Add(node);
-                    }
-                    else
-                    {
-                        treeView1.Nodes.Add(node);
-                    }
-                }
-            }
-        }
-    }
+		}
+	}
 }
