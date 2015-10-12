f628cdc52240f0c26ced932bd2cef2d87b15f300
bartekordek <bartekordek@gmail.com>
2015-10-12 12:30:57 +0200

    Added DbBuilder class.

---
+++ Ultimate Management Tool/Ultimate Management Tool/DbBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbBuilder
+{
+	class CDbBuilder
+	{
+		static public void CreateEmptyDb( DBManager.CDbManager dbManager, string dbPath = "mainDataBase.db" )
+		{
+			CreateFile( dbManager, dbPath );
+			CreateConnection( dbManager );
+			CreateTables( dbManager );
+        }
+
+		static private void CreateFile( DBManager.CDbManager dbManager, string dbPath )
+		{
+			dbManager.CreateNewDbFile( dbPath );
+		}
+
+		static private void CreateConnection( DBManager.CDbManager dbManager)
+		{
+			dbManager.CreateConnection();
+		}	
+
+		static private void CreateTables( DBManager.CDbManager dbManager )
+		{
+			dbManager.ExecuteQuery( 
+				"CREATE TABLE UserACtivities " + 
+				"(" +
+					"id INTEGER NOT NULL AUTO_INCREMENT " + 
+					"userId INTEGER NOT NULL" +
+				")" 
+				);
+		}
+	}
+}
