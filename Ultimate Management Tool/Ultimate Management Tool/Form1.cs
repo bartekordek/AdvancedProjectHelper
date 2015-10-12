d9a09718ce5527107265dfbb2b36477bd3463b13
bartekordek <bartekordek@gmail.com>
2015-10-12 12:32:41 +0200

    Renamed Ultimate_Management_Tool to UltimateManagementTool.

--- Ultimate Management Tool/Ultimate Management Tool/Form1.cs
+++ Ultimate Management Tool/Ultimate Management Tool/Form1.cs
@@ -7,19 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 
-namespace Ultimate_Management_Tool
+namespace UltimateManagementTool
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+			dbManager = new DBManager.CDbManager();
         }
 
 		private void btnLoadDb_Click( object sender, EventArgs e )
 		{
-
+			
 		}
+
+		private void btnCreateNewDb_Click( object sender, EventArgs e )
+		{
+			DbBuilder.CDbBuilder.CreateEmptyDb( dbManager );
+		}
+
+		private DBManager.CDbManager dbManager;
 	}
 }
